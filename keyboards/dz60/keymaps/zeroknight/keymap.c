0733ba16674b628aabbae195d1d60da10848493b
ZeroKnight <xzeroknightx@gmail.com>
2024-05-04 22:18:07 -0700

    feat(dz60): Update keymap
    Updated DZ60 keymap to reflect some more recent thoughts after
    configuring my new Molly60. I've fully committed to LCtrl being where
    Caps Lock is. Turns out, I've gravitated pretty close to a HHKB layout
    this whole time.
    
    Some keymaps were shuffled a bit. Notably, all cursor navigation is
    centered around the WASD cluster. Home/End were moved to Q/E, and
    PgUp/PgDn were moved to R/F. Word movement can easily be done by just
    holding LCtrl.
    
    Caps Lock is now on the Media Layer, positioned where it normally is.
    Vol+/Vol- is now W/S on the Media Layer in order to mimic the arrows.

--- keyboards/dz60/keymaps/zeroknight/keymap.c
+++ keyboards/dz60/keymaps/zeroknight/keymap.c
@@ -34,21 +34,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT_60_tsangan(
         KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
         KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        MY_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+        KC_LCTL,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSPO,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSPC, FN_1,
         KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                                      KC_RALT,          FN_2,    KC_RCTL
     ),
     [_EL] = LAYOUT_60_tsangan(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
-        _______,          WORDL,   KC_UP,   WORDR,   _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______,
-        _______,          KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  _______,          _______, _______,
+        _______,          KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______,
+        _______,          KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
         _______, _______,          _______,                   _______,                                     KC_HYPR,          MO(_CL), _______
     ),
     [_ML] = LAYOUT_60_tsangan(
         KC_MSTP, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,           KC_EJCT,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BRID, KC_BRIU, _______,
-        _______,          _______, _______, _______, KC_FIND, _______, _______, _______, _______, _______, KC_VOLU, KC_VOLD, _______,
+        _______,          _______, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______, KC_BRID, KC_BRIU, _______,
+        KC_CAPS,          _______, KC_VOLD, _______, KC_FIND, _______, _______, _______, _______, _______, _______, _______, _______,
         _______,          _______, _______, KC_CALC, _______, _______, TG(_NL), KC_MUTE, KC_MPRV, KC_MNXT, _______,          _______, MO(_CL),
         _______, _______,          _______,                   KC_MPLY,                                     _______,          _______, _______
     ),
