7db54daaea56561e5760fbe7a0fd766f67660b02
ZeroKnight <xzeroknightx@gmail.com>
2024-05-05 02:20:45 -0700

    feat(dz60): Update Extension layer
    - Esc is `KC_GRV`
    - `I` is Insert
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch main
    # Your branch is ahead of 'origin/main' by 1 commit.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	modified:   keyboards/dz60/keymaps/zeroknight/keymap.c
    #
    # Changes not staged for commit:
    #	modified:   keyboards/dz60/keymaps/zeroknight/config.h
    #	modified:   keyboards/dz60/keymaps/zeroknight/rules.mk
    #

--- keyboards/dz60/keymaps/zeroknight/keymap.c
+++ keyboards/dz60/keymaps/zeroknight/keymap.c
@@ -36,8 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                                      KC_RALT,          FN_2,    KC_RCTL
     ),
     [_EL] = LAYOUT_60_tsangan(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
-        _______,          KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS, _______,
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
+        _______,          KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, KC_INS,  _______, KC_PSCR, KC_SCRL, KC_PAUS, _______,
         _______,          KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______, _______, _______, _______, _______, _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
         _______, _______,          _______,                   _______,                                     KC_HYPR,          MO(_CL), _______
