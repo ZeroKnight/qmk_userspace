2c4146e50bbdecba1c69d0399960911d4f98f1c3
ZeroKnight <xzeroknightx@gmail.com>
2024-05-04 23:02:06 -0700

    refactor(dz60): Use new keycode names
    Sometime after I originally made this keymap, QMK refactored some of
    the naming conventions for various keycodes.

--- keyboards/dz60/keymaps/zeroknight/keymap.c
+++ keyboards/dz60/keymaps/zeroknight/keymap.c
@@ -32,15 +32,15 @@ enum KeyboardLayers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT_60_tsangan(
-        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
+        QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
         KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_LCTL,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LSPO,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSPC, FN_1,
+        SC_LSPO,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          SC_RSPC, FN_1,
         KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                                      KC_RALT,          FN_2,    KC_RCTL
     ),
     [_EL] = LAYOUT_60_tsangan(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
-        _______,          KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______,
+        _______,          KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS, _______,
         _______,          KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______, _______, _______, _______, _______, _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
         _______, _______,          _______,                   _______,                                     KC_HYPR,          MO(_CL), _______
@@ -53,8 +53,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,          _______,                   KC_MPLY,                                     _______,          _______, _______
     ),
     [_CL] = LAYOUT_60_tsangan(
-        KC_SLEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EEP_RST,
-        _______,          RGB_TOG, RGB_MOD, RGB_RMOD,_______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,
+        KC_SLEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EE_CLR,
+        _______,          RGB_TOG, RGB_MOD, RGB_RMOD,_______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,
         _______,          RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______, _______, _______, _______, _______, _______, _______, _______,
         _______,          RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, _______, _______, _______, _______, _______, _______,          _______, _______,
         _______, GUI_TOG,          _______,                   _______,                                     _______,          _______, _______
